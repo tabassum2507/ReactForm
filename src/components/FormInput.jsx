b77d4f35af165f75d3f9f83eba4aaf94f05afdd0
tabassum2507 <10tabassum2@gmail.com>
2021-12-02 23:22:57 +0530

    app files added

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./FormInput.css"
+
+const FormInput = () => {
+    return (
+        <div className="formInput">
+
+            <label>Username</label>
+            <input></input>
+            
+        </div>
+    )
+}
+
+export default FormInput
